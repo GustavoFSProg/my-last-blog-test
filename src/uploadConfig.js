0b07a1aede722b3a3fa9a6225ce988f1a1305327
Gustavo <gustavo.prog40@gmail.com>
2022-05-26 16:51:11 -0300

    trew

--- src/uploadConfig.js
+++ src/uploadConfig.js
@@ -4,7 +4,7 @@ import path from 'path'
 const uploadConfig = {
   // eslint-disable-next-line new-cap
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    // destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename(_req, file, cb) {
       const [name] = file.originalname.split('.')
       const filename = `${name}.jpg`
