ca46e439db51812bf583f65fa1ba7fdf95863cfd
Gustavo <gustavo.prog40@gmail.com>
2022-05-26 16:16:07 -0300

    asdfasdf

--- src/routes.js
+++ src/routes.js
@@ -12,14 +12,14 @@ routes.get('/', (req, res) => {
   return res.status(200).send({ msg: 'API version 1.0' })
 })
 
-routes.get('/get-all', postController.getAll)
-routes.post('/register', upload.single('image'), postController.register)
-routes.put('/update/:id', upload.single('image'), postController.update)
-routes.put('/likes/:id', postController.updateLikes)
-routes.put('/views/:id', postController.updateViews)
-routes.delete('/del/:id', postController.deleted)
+// routes.get('/get-all', postController.getAll)
+// routes.post('/register', upload.single('image'), postController.register)
+// routes.put('/update/:id', upload.single('image'), postController.update)
+// routes.put('/likes/:id', postController.updateLikes)
+// routes.put('/views/:id', postController.updateViews)
+// routes.delete('/del/:id', postController.deleted)
 
-routes.post('/comments/:id', commentsController.comments)
-routes.get('/commentsGetAll', commentsController.getAllComments)
+// routes.post('/comments/:id', commentsController.comments)
+// routes.get('/commentsGetAll', commentsController.getAllComments)
 
 export default routes
